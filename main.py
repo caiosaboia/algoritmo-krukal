0987f723b8a1b9045bef88ace5df15eebe2bcd58
Caio Saboia <caiorubems@gmail.com>
2023-11-01 19:01:58 -0300

    Ajustes de nomeclaturas

--- main.py
+++ main.py
@@ -27,23 +27,23 @@ components = {node: node for node in G.nodes}
 edges = sorted(G.edges(data=True), key=lambda x: x[2]['weight'])
 
 # Função para encontrar o componente de um nó
-def find_component(node):
+def procuraComponente(node):
     if components[node] != node:
-        components[node] = find_component(components[node])
+        components[node] = procuraComponente(components[node])
     return components[node]
 
 # Função para unir dois componentes
-def union_components(component1, component2):
-    root1 = find_component(component1)
-    root2 = find_component(component2)
+def uniao(component1, component2):
+    root1 = procuraComponente(component1)
+    root2 = procuraComponente(component2)
     components[root1] = root2
 
 # Algoritmo de Kruskal
 for edge in edges:
     u, v, data = edge
-    if find_component(u) != find_component(v):
+    if procuraComponente(u) != procuraComponente(v):
         mst_edges.append((u, v, data))
-        union_components(u, v)
+        uniao(u, v)
 
 # Crie um grafo com as arestas da árvore geradora mínima
 mst = nx.Graph(mst_edges)
@@ -58,6 +58,7 @@ labels = nx.get_edge_attributes(G, 'weight')
 nx.draw_networkx_edge_labels(G, pos, edge_labels={(k[0], k[1]): v for k, v in labels.items()})
 plt.title("Grafo Original")
 plt.savefig("grafoOriginal.png")
+plt.show()
 
 
 # Desenhe a árvore geradora mínima sem rótulos de peso para arestas que não fazem parte dela
@@ -67,5 +68,6 @@ nx.draw(mst, pos, with_labels=True)
 nx.draw_networkx_edge_labels(mst, pos, edge_labels=mst_labels)
 plt.title("Árvore Geradora Mínima")
 plt.savefig("arvore geradora minima.png")
+plt.show()
 
 print("Finalizado")
